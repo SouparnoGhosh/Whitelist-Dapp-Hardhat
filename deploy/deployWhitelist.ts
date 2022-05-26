5ad200879476b27b2a764d676997e42861cb1363
SouparnoGhosh <ghosh99souparno@gmail.com>
2022-05-26 10:33:14 +0530

    Deployed contract

--- deploy/deployWhitelist.ts
+++ deploy/deployWhitelist.ts
@@ -4,18 +4,22 @@ import { DeployFunction } from "hardhat-deploy/dist/types";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts } = hre;
-  const { deploy } = deployments;
+  const { deploy, log } = deployments;
 
   const { deployer } = await getNamedAccounts();
 
-  await deploy("EtherWallet", {
+  const deployedWhitelistContract = await deploy("Whitelist", {
     // <-- name of the deployment
-    contract: "EtherWallet", // <-- name of the contract/artifact(more specifically) to deploy
+    contract: "Whitelist", // <-- name of the contract/artifact(more specifically) to deploy
     from: deployer, // <-- account to deploy from
-    args: [], // <-- contract constructor arguments. Here it has nothing
+    args: [10], // <-- contract constructor arguments. Here it has nothing
     log: true, // <-- log the address and gas used in the console
   });
+
+  log(
+    `Deployed Whitelist Contract Address: ${deployedWhitelistContract.address}`
+  );
 };
 
 export default func;
-func.tags = ["EtherWallet"];
+func.tags = ["Whitelist"];
